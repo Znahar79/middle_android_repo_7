9695b0cd87708987dfbdb87ba8389afb07c0ac6d
Znahar79 <znakhar1998@yandex.ru>
2026-03-08 22:49:09 +0300

    Task 1 and 2: added content descriptions, setup minimumInteractiveComponentSize for ProductCard

--- app/src/main/java/ru/yandexpraktikum/marketplace/ui/screens/MainScreen.kt
+++ app/src/main/java/ru/yandexpraktikum/marketplace/ui/screens/MainScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -28,6 +27,7 @@ import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -40,6 +40,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -78,7 +79,8 @@ fun MainScreen(onProductClick: (Int) -> Unit) {
                 .fillMaxSize()
                 .padding(paddingValues)
         ) {
-            //val searchBarDescription = stringResource(R.string.searchbar_description)
+            val searchBarDescription = stringResource(R.string.searchbar_description)
+
             SearchBar(
                 query = searchQuery,
                 onQueryChange = { searchQuery = it },
@@ -93,6 +95,9 @@ fun MainScreen(onProductClick: (Int) -> Unit) {
                 },
                 placeholder = {
                     Text(
+                        modifier = Modifier.semantics {
+                            contentDescription = searchBarDescription
+                        },
                         text = stringResource(R.string.search_products),
                         color = Color(0xFFAAAAAA)
                     )
@@ -150,7 +155,7 @@ fun ProductCard(
             ) {
                 AsyncImage(
                     model = product.imageUrl,
-                    contentDescription = null,
+                    contentDescription = product.description,
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(150.dp),
@@ -183,16 +188,20 @@ fun ProductCard(
                         color = Color(0xFFAAAAAA)
                     )
                 }
-                //val actionDescription = stringResource(R.string.add_product_to_cart, product.name)
+
+                val actionDescription = stringResource(R.string.add_product_to_cart, product.name)
+
                 Icon(
                     Icons.Default.ShoppingCart,
                     contentDescription = stringResource(R.string.add_to_cart),
                     tint = Color(0xFFAAAAAA),
                     modifier = Modifier
-                        .clickable {
+                        .clickable(
+                            onClickLabel = actionDescription
+                        ) {
                             onAddToCart()
                         }
-                        .size(16.dp)
+                        .minimumInteractiveComponentSize()
                 )
             }
         }
